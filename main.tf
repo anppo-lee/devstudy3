cb5c40b0e4caf34411215ae7a6e4f7b73d86f30b
anppo-lee <100008102+anppo-lee@users.noreply.github.com>
2022-05-15 22:27:56 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,3 +30,28 @@ resource "aws_instance" "app_server" {
     Name = "ExampleAppServerInstance"
   }
 }
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+
+  ingress {
+    description      = "SSH from me"
+    from_port        = 22
+    to_port          = 222
+    protocol         = "tcp"
+    cidr_blocks      = ["222.121.135.254"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
